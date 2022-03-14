6f62ef2cd6eb04231139d591cc42ba144996b346
kacperdziobczynski <dzkacp@gmail.com>
2022-03-14 17:09:52 +0100

    Import MapView

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -3,13 +3,16 @@ import { StyleSheet } from 'react-native';
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
+import MapView from 'react-native-maps';
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+        <MapView
+            style={{height: "100%", width: "100%"}}
+            showsUserLocation={true}
+        />
+
     </View>
   );
 }
