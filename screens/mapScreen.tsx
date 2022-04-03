630ba779ebb60a79307be3dbebe6d8936419b420
Kacper Dzióbczyński <dzkacp@gmail.com>
2022-04-03 14:23:29 +0200

    Import map view (#4)

--- screens/mapScreen.tsx
+++ screens/mapScreen.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import { View, Text } from 'react-native';
 import { DefaultScreenProps } from '../common/DefaultScreenProps';
 import { globalStyles } from '../styles/global';
+import MapView from 'react-native-maps';
 
 export default function MapScreen({ navigation }: DefaultScreenProps) {
   return (
     <View style={globalStyles.container}>
-      <Text>Map Screen</Text>
+        <MapView
+            style={{height: "100%", width: "100%"}}
+            showsUserLocation={true}
+        />
     </View>
   );
 }
