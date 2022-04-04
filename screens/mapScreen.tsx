aa1e841743d769c3d14f6a44e98e71f1a4b1f69f
kacperdziobczynski <dzkacp@gmail.com>
2022-04-04 18:51:20 +0200

    Add custom components

--- screens/mapScreen.tsx
+++ screens/mapScreen.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
-import { View, Text } from 'react-native';
 import { DefaultScreenProps } from '../common/DefaultScreenProps';
-import { globalStyles } from '../styles/global';
 import MapView from 'react-native-maps';
 
 export default function MapScreen({ navigation }: DefaultScreenProps) {
   return (
-    <View style={globalStyles.container}>
         <MapView
             style={{height: "100%", width: "100%"}}
             showsUserLocation={true}
         />
-    </View>
   );
 }
