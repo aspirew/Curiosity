63b0a58b4c4a3196648c102e2ce2d2a4c32ec478
kacperdziobczynski <dzkacp@gmail.com>
2022-06-03 18:42:30 +0200

    Add event component

--- screens/mapScreen.tsx
+++ screens/mapScreen.tsx
@@ -1,55 +1,138 @@
-import React, { useRef, useState } from 'react';
-import { DefaultScreenProps } from '../common/DefaultScreenProps';
-import MapView, { MapEvent, Marker } from 'react-native-maps';
-import { StyleSheet, View } from 'react-native';
-import SelectedLocationComponent from '../components/selectedLocationComponent';
+import React, {useEffect, useRef, useState} from 'react';
+import {DefaultScreenProps} from '../common/DefaultScreenProps';
+import MapView, {LatLng, MapEvent, Marker} from 'react-native-maps';
+import {StyleSheet, View} from 'react-native';
+import {getEvent, getEvents} from "../services/dbService";
+import BottomSheet from "reanimated-bottom-sheet";
+import {Layout} from "@ui-kitten/components";
+import Animated from "react-native-reanimated";
+import Event from "../models/event";
+import EventComponent from "../components/eventComponent";
 
-
-type marker = {
-  latitude: number,
-  longitude: number
+type EventMarker = {
+    id: string
+    title: string
+    description: string
+    coordinate: LatLng
 }
 
+export default function MapScreen({navigation}: DefaultScreenProps) {
+    const mapRef = useRef(null);
+    const [events, setEvents] = useState<Event[]>([]);
+    const [eventMarkers, setEventMarkers] = useState<EventMarker[]>([]);
+    const [pickedEvent, setPickedEvent] = useState<Event | undefined>(undefined);
 
-export default function MapScreen({ navigation }: DefaultScreenProps) {
-  const mapRef = useRef(null);
-  const [marker, setMarker] = useState<marker | undefined>(undefined);
+    useEffect(() => {
+        const fetchEvents = async () => {
+            const eventDocs = await getEvents();
+            eventDocs.forEach(doc => {
+                events.push(doc)
+            })
+            setEvents(events)
+            setEventMarkers(prepareEventMarker(eventDocs))
+        }
 
-  function onMapPress(e: MapEvent) {
-    setMarker(e.nativeEvent.coordinate)
-  }
+        fetchEvents();
+    }, [events])
 
-
-  return (
-    <View style={styles.container}>
-      <MapView
-        ref={mapRef}
-        style={styles.map}
-        onPress={(e) => onMapPress(e)}
-      >
-      {
-        marker ?
-        <Marker coordinate={marker}></Marker>
-        : null
-      }
-      </MapView>
-    {
-      marker ?
-      <SelectedLocationComponent coordinates={marker} navigation={navigation}/> 
-      : null
+    function prepareEventMarker(eventDocs: Event[]) {
+        console.log(eventDocs)
+        return eventDocs.map(eventDoc => {
+            return {
+                id: eventDoc.id,
+                title: eventDoc.name,
+                // description: eventDoc.description,
+                coordinate: {
+                    latitude: !!eventDoc.latitude ? eventDoc.latitude : eventDoc.location.latitude,
+                    longitude: !!eventDoc.longitude ? eventDoc.longitude : eventDoc.location.longitude,
+                }
+            }
+        })
     }
-    </View>
-  );
+
+    async function clickOnMarker(e: MapEvent) {
+        setPickedEvent(null)
+        const event = await getEvent(e.id)
+        setPickedEvent(event)
+        bs.current.snapTo(0)
+    }
+
+    const renderInner = () => (
+        <EventComponent {...pickedEvent}/>
+    );
+
+    const bs = React.createRef();
+    const fall = new Animated.Value(1);
+
+    return (
+        <Layout style={{flex: 1}}>
+            <View style={styles.container}>
+
+                <MapView
+                    ref={mapRef}
+                    style={styles.map}
+                >
+                    {eventMarkers.map((marker, index) => (
+                        <Marker
+                            key={marker.id}
+                            identifier={marker.id}
+                            coordinate={marker.coordinate}
+                            title={marker.title}
+                            description={marker.description}
+                            onPress={e => {
+                                clickOnMarker(e.nativeEvent);
+                                bs.current.snapTo(0)
+
+                            }}
+                        />
+                    ))}
+                </MapView>
+
+            </View>
+            <BottomSheet
+                style={styles.bottomSheet}
+                ref={bs}
+                snapPoints={[500, 25]}
+                renderContent={renderInner}
+                initialSnap={1}
+                callbackNode={fall}
+                enabledGestureInteraction={true}
+            />
+        </Layout>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    ...StyleSheet.absoluteFillObject,
-    flex: 1,
-    justifyContent: "flex-end",
-    alignItems: "center",
-  },
-  map: {
-    ...StyleSheet.absoluteFillObject,
-  }
+    container: {
+        flex: 1,
+        justifyContent: "flex-end",
+        alignItems: "center",
+    },
+    bottomSheet: {
+        justifyContent: "flex-end",
+        alignItems: "center",
+        borderColor: 'black',
+        borderWidth: 5,
+        width: '100%'
+    },
+    map: {
+        ...StyleSheet.absoluteFillObject,
+    },
+    panel: {
+        padding: 20,
+        backgroundColor: '#FFFFFF',
+        paddingTop: 20,
+        width: '100%',
+    },
+    panelTitle: {
+        textAlign: "left",
+        fontSize: 27,
+        height: 35
+    },
+    panelSubtitle: {
+        fontSize: 14,
+        color: 'gray',
+        height: 30,
+        marginBottom: 10,
+    },
 });
